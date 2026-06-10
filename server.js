91cbffc1657811f27cf9db351e86d480886b4237
NielsVandenBroeck <nielsvandenbroeck11@gmail.com>
2026-06-10 15:39:36 +0200

    csp problem fix

--- server.js
+++ server.js
@@ -50,9 +50,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 // ─── HELPERS ──────────────────────────────────────
 
