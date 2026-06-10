79a4c38a0ebe4a5d97984739371c04db575696d7
NielsVandenBroeck <nielsvandenbroeck11@gmail.com>
2026-06-10 14:37:19 +0200

    change dist to public

--- server.js
+++ server.js
@@ -42,9 +42,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 // ─── HELPERS ──────────────────────────────────────
 
