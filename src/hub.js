a4f5bd66ab947248dce8297c84029dbc509aac79
NielsVandenBroeck <nielsvandenbroeck11@gmail.com>
2026-06-30 22:22:54 +0200

    fix button redirect

--- src/hub.js
+++ src/hub.js
@@ -1,3 +1,4 @@
+// hub.js
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 // Use the same Client ID from your other game files
@@ -7,4 +8,13 @@ const sdk = new DiscordSDK(DISCORD_CLIENT_ID);
 // Tell Discord the menu is alive
 sdk.ready().then(() => {
     console.log("[HUB] Discord SDK ready!");
+});
+
+// Event listeners moved here to comply with CSP
+document.getElementById('btn-chroma').addEventListener('click', () => {
+    window.location.href = '/chroma.html';
+});
+
+document.getElementById('btn-flagle').addEventListener('click', () => {
+    window.location.href = '/flagle.html';
 });
